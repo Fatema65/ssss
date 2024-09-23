47201b720e32451ac2855cb8e7145afed230a92c
Fatema Akther <sumafatema10@gmail.com>
2024-09-23 11:28:09 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+  
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="home_style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <!-- google fonts -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+   <header>
+      <div>
+         <p>developer</p>
+      </div>
+     
+         <nav   class="menu">
+            <ul>
+               <li>home</li>
+               <li>service</li>
+               <li>contact</li>
+            </ul>
+         </nav>
+
+
+
+   </header>
+   <!-- first section -->
+   <section class="first-section">
+   <p>Frelancer</p>
+   <h1>website design</h1>
+  <button class="global-bttn">my work</button>
+</section>
+<!-- 2nd section -->
+
+<section class="global-section">
+   <h2 class="global-headding"> About me</h2>
+   <p class=global-head-p>Delicious meals made with clean ingredients</p>
+   <div class="me">
+      <div>
+         <img src="./images/man.PNG">
+
+      </div>
+      <div class="sub-div2">
+         <h2>Quick me</h2>
+
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people. The wealth-tracking platform provides ongoing updates on the net worth and ranking of each individual confirmed by Forbes to be a billionaire. The value of individuals’ public holdings are updated every 5 minutes when respective stock markets are open </p>
+         <button class="global-bttn">Here me</button>
+
+      </div>
+      
+
+
+   </div>
+   
+
+</section>
+
+<!-- this is 3rd section -->
+
+
+<section class="global-section">
+   <h2 class="global-headding"> service</h2>
+   <p class=global-head-p>Delicious meals made with clean ingredients</p>
+    <div class="grid-container">
+       
+       <div class="grid-singel">
+       <i class="fa-sharp-duotone fa-solid fa-laptop-code icon-size"></i>
+         <h2>Web design</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+  
+
+
+   <div class="grid-singel">
+      <i class="fa-sharp fa-regular fa-image icon-size"></i>
+         <h2>Photoshop</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+   
+
+   <div class="grid-singel">
+
+         <h2>Graphics</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+
+        <div class="grid-singel">
+         <h2>video editing</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+
+        <div class="grid-singel">
+         <h2>facebook marketing</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+        <div class="grid-singel">
+         <h2>SEO</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+    </div>
+   
+
+</section>
+
+<!-- 4th section -->
+<section class="global-section">
+   <h2 class="global-headding"> work section</h2>
+   <p class=global-head-p>Delicious meals made with clean ingredients</p>
+    <div class="grid-container">
+       
+       <div class="grid-singel">
+      <img class="image-wi" src="./images/w.PNG">
+         <h2>Web design</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+  
+
+
+   <div class="grid-singel">
+     <img class="image-wi" src="./images/p.PNG">
+         <h2>Photoshop</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+   
+
+   <div class="grid-singel">
+<img class="image-wi" src="./images/g.PNG">
+         <h2>Graphics</h2>
+         <p>Forbes' Real-Time Billionaires rankings tracks the daily ups and downs of the world’s richest people.</p>
+       </div>
+
+        
+        
+    </div>
+   
+
+</section>
+<section class="global-section">
+   <h2 class="global-headding"> testemonial</h2>
+   <form>
+   <label> input name</label>
+   <input type="text" placeholder="input name"><br>
+   <label> input email</label>
+   <input type="text" placeholder="input email"><br>
+   <label> input message</label><br>
+  <textarea name="message"></textarea><br>
+  <input class="global-bttn"type="button" value="submit">
+</form>
+</section>
+
+</body>
+
+</html>
+
